a747a2e6bcc99bf2a7c9e79ecba511b085cb6305
Marie Klisnick <marie63.klisnick@gmail.com>
2024-09-24 12:18:48 +0200

    Exercice User

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 import { useState } from 'react'
-import User from './user'
+import User from './User'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const users = [
+    {
+      name: 'Marie',
+      lastname: 'Ksnk',
+      age: 27,
+      profession: 'Webdesigneur'
+    },
+    {
+      name: 'John',
+      lastname: 'Doe',
+      age: 30,
+      profession: 'Software Engineer'
+    },
+    {
+      name: 'Alice',
+      lastname: 'Smith',
+      age: 25,
+      profession: 'Graphic Designer'
+    },
+    {
+      name: 'Robert',
+      lastname: 'Brown',
+      age: 40,
+      profession: 'Project Manager'
+    }
+  ];
 
   return (
     <>
-    <User />
+
+    {users.map((user, index) => (
+      <User key={index} {...user} />
+    ))}
     </>
   )
 }
--- src/User.jsx
+++ src/User.jsx
@@ -1,29 +1,12 @@
 
 
-const User = () => {
-
-    let info = {
-    Name: "Marie",
-    LastName: "Klisnick",
-    Age: 27
-    }
-
-    const isAuthenticated = true
-
-    return (<>
-    <h1>Hello my name is :  {info.Name} {info.LastName}, j'ai {info.Age} ans</h1>
-    <p>Je suis webdesigneuse & apprentie developpeuse web</p>
-    {isAuthenticated ? (
+const User = ({name, lastname, age, profession}) => {
+    return (
         <>
-        <h1>Welcome {info.Name}</h1>
+        <h3>Hello my name is {name}</h3>
+        <p>Age : {age}</p>
+        <p>Profession : {profession}</p>
         </>
-    ) : (
-        <>
-        <h1>Please sign in</h1>
-        <button>Sign in</button>
-        </>
-    )}
-    </>
     )
 }
 
