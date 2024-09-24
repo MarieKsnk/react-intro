237c722f3ab1033d862d56dba04383f61582c1c1
Marie Klisnick <marie63.klisnick@gmail.com>
2024-09-24 15:19:56 +0200

    Button increment/decrement

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,11 +1,37 @@
+import {useState} from 'react'
+
+
 const Button = () => {
 
-    const clickMe = () => {
-        console.log("Hello World")
+    const [count, setCount] = useState(0)
+// const [isOpen, setIsOpen] = useState(false)
+// const [name, setName] = useState('')
+
+    const incrementCounter = () => {
+        setCount(count + 5)
+    }
+
+    const decrementCounter = () => {
+        setCount(count - 5)
     }
 
     return(
-        <button onClick={clickMe}>Click me</button>
+        <>
+        <h1>Counter : {count}</h1>
+        <button onClick={incrementCounter}>+ 5</button>
+        <button onClick={decrementCounter}>- 5</button>
+
+        {/* {isOpen ? <p>Wlecome you can order what you want</p> : <p>Sorry we are close</p>}
+        <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? "Close" : "Open"}</button>
+
+        <br />
+
+        {name}
+
+        <form action="">
+            <input type="text" onChange={e => setName(e.target.value)}/>
+        </form> */}
+        </>
     )
 }
 
